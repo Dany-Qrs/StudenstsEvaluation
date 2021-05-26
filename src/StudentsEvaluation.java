371635e3b435cc0205a17ccf32f7667fe92a65a3
Dani2 <daniela18122012@gmail.com>
2021-05-26 14:00:28 -0600

    commit 1

--- src/StudentsEvaluation.java
+++ src/StudentsEvaluation.java
@@ -4,4 +4,7 @@
 min, max, avg, most repeated, less repeated.
  We need to input your name and your evaluation result*/
 public class StudentsEvaluation {
+    public static void main(String[] args){
+        
+    }
 }

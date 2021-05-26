f3cc96d8171b6d232c7fee9ea53a409f5ebe087a
Dany-Qrs <69737289+Dany-Qrs@users.noreply.github.com>
2021-05-26 13:34:42 -0600

    Update StudentsEvaluation.java

--- src/StudentsEvaluation.java
+++ src/StudentsEvaluation.java
@@ -1,2 +1,7 @@
+/*Program name: Students evaluation*/
+//Author´s name:  Dani Cruz
+/*Present statistics for a section of 20 students:
+min, max, avg, most repeated, less repeated.
+ We need to input your name and your evaluation result*/
 public class StudentsEvaluation {
 }

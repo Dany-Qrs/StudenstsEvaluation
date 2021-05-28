e4f10cb1f5c39235e3641a1fc73d37f52d6bc117
Dani2 <daniela18122012@gmail.com>
2021-05-27 19:14:59 -0600

    metodos

--- src/StudentsEvaluation.java
+++ src/StudentsEvaluation.java
@@ -8,8 +8,10 @@ import java.io.FileWriter;
 import java.io.File;
 import java.io.IOException;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class StudentsEvaluation {
+
     public static void welcomePrint(){
         StringBuilder myWelcome = new StringBuilder();
         myWelcome.append(" _  _    ____    ___      _     ___     ____\n");
@@ -21,25 +23,10 @@ public class StudentsEvaluation {
         myWelcome.append("|_|\\_\\  |____|  |____/   |_|   \\____/  |____|\n");
         System.out.println(myWelcome);
     }
-    public static void infoRequest(){
-
-        System.out.println("");
-    }
-    public static void main(String[] args){
-        //printWelcome();
+    public static void infoRequest(int numStudents,List<String> names, List<Double> scores){
         Scanner scan = new Scanner(System.in);
-        System.out.print("Cantidad de alumnos: \t");
-        int numStudents = scan.nextInt();
-        List<String> names = new ArrayList<>();
-        List<Double> scores = new ArrayList<>();
-        Double minScore;
-        Double maxScore;
-        Double avgScore;
-        Double mostRepeatScore;
-        Double lessRepeatScore;
-        int iterator = 1;
-
-        while (iterator <= numStudents){
+        int iterator = 0;
+        while (iterator < numStudents){
             System.out.println("_________________________________________________");
             System.out.print("Estudiante: ");
             names.add(scan.next());
@@ -47,6 +34,54 @@ public class StudentsEvaluation {
             scores.add(scan.nextDouble());
             iterator ++;
         }
+    }
+    public static List arraySort(int numStudents, List<Double> scores){
+        List<Double> scoreSort = new ArrayList<>();
+        Double[] scoreAux = new Double[numStudents];
+        scoreAux= scores.toArray(scoreAux);
+        for(int i=0; i< scoreAux.length-1; i++){
+            for(int j=i+1; j< scoreAux.length; j++){
+                if(scoreAux[i] > scoreAux[j]){
+                    double aux = scoreAux[i];
+                    scoreAux[i]= scoreAux[j];
+                    scoreAux[j] = aux;
+                }
+            }
+            scoreSort.add(scoreAux[i]);
+        }
+        return scoreSort;
+    }
+    public static double avgFind(int numStudents,List<Double> scores){
+        double avg = 0;
+        for(double score: scores){
+            avg += score;
+        }
+        return avg / numStudents;
+    }
+    public static StringBuilder repeatFind(List<Double> scores){
+        StringBuilder bodyInfo = new StringBuilder();
+        Map<Double, Integer> repeatScore = new HashMap<>();
+        int maxRepeat, minRepeat;
+        for(double aux: scores){
+            if(repeatScore.containsKey(aux)){
+                repeatScore.put(aux, repeatScore.get(aux)+1);
+            }else{
+                repeatScore.put(aux, 1);
+            }
+        }
+        maxRepeat = Collections.max(repeatScore.values());
+        minRepeat = Collections.min(repeatScore.values());
+        List<Double> maxScoreRepeat = repeatScore.entrySet().stream().filter(x -> Objects.equals(x.getValue(), maxRepeat))
+                                    .map(Map.Entry::getKey).collect(Collectors.toList());
+        List<Double> minScoreRepeat = repeatScore.entrySet().stream().filter(x -> Objects.equals(x.getValue(), minRepeat))
+                .map(Map.Entry::getKey).collect(Collectors.toList());
+        bodyInfo.append("Most repeat: "+maxScoreRepeat+"\t");
+        bodyInfo.append("Less repeat: "+minScoreRepeat+ "\n");
+        return bodyInfo;
+    }
+   /* public static void general(){
+        Map<Double, Integer> repeatScore = new HashMap<>();
+
 
         System.out.println("¿Desea guardar los datos en un archivo de texto? presiona y/n");
         if(scan.next().equals("y")){
@@ -54,21 +89,17 @@ public class StudentsEvaluation {
                 FileWriter dataFile = new FileWriter("nuevo.txt");
                 Iterator<String> name = names.iterator();
                 Iterator<Double> score = scores.iterator();
-                Double acumAvg = 0.0;
-                Double[] mostRepeat;
-                for(int i=0; i< scores.size(); i++){
-                    System.out.println(scores.get(i));
-                }
+
                 StringBuilder bodyInfo = new StringBuilder();
                 bodyInfo.append("Student Score\n");
                 while (name.hasNext() && score.hasNext()){
                     bodyInfo.append(name.next()+" ");
                     bodyInfo.append(score.next()+"\n");
                 }
-                avgScore = acumAvg / numStudents;
-
                 bodyInfo.append("\nSTADISTICS\n");
                 bodyInfo.append("Avg: "+avgScore+"\t");
+                bodyInfo.append("Min: "+minScore+"\t");
+                bodyInfo.append("Max: "+maxScore+"\t");
 
                 dataFile.write(bodyInfo.toString());
                 dataFile.close();
@@ -76,5 +107,24 @@ public class StudentsEvaluation {
                 e.printStackTrace();
             }
         }
+    }*/
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        List<Double> scores = new ArrayList<>();
+        List<Double> scoreSort = new ArrayList<>();
+        List<String> names = new ArrayList<>();
+        String mostLessScore;
+        double minScore, maxScore, avgScore;
+        int numStudents;
+        System.out.print("Cantidad de alumnos: ");
+        numStudents = scan.nextInt();
+        infoRequest(numStudents, names, scores);
+        scoreSort = arraySort(numStudents, scores);
+        maxScore = scoreSort.get(scoreSort.size()-1);
+        minScore = scoreSort.get(0);
+        avgScore = avgFind(numStudents, scores);
+        mostLessScore = repeatFind(scores).toString();
+
+        System.out.println(maxScore);
     }
 }
